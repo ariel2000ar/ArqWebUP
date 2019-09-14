b2b1e88906defcf6b71ce4dee55d01f4f11ce337
urisega <53956036+urisega@users.noreply.github.com>
2019-09-13 21:06:16 -0300

    Add files via upload

--- tycarq.js
+++ tycarq.js
@@ -1,14 +1,16 @@
 const express    = require('express');
 const moment     = require('moment');
 const mysql	 = require('mysql');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root', 
 	password: '', 
-	dataBase: 'tpup'
+	database: 'tpup'
 });
 	
 connection.connect(function(error){
@@ -21,49 +23,92 @@ connection.connect(function(error){
 
 
 //DEPORTE
-app.get('/deportes', async (req, res) => {
 
-    connection.query("SELECT * FROM tpup.deportes ORDER BY id_deporte", function(error, rows, fields){
+app.get('/deportes', async (req, res) => {
+    if (!req.body.id){
+		connection.query("SELECT * FROM deportes ORDER BY id_deporte", function(error, rows, fields){
+			if (!!error){
+				console.log(error);
+			}else{
+				res.json(rows);
+			}
+		});
+	}else{
+		connection.query("SELECT * FROM deportes where id_deporte = "+req.body.id, function(error, rows, fields){
+			if (!!error){
+				console.log(error);
+			}else{
+				res.json(rows);
+			}
+		});		
+	}	
+});
+
+/*
+app.post('/deportes2', async (req, res) => {
+    if (!req.body.desc_deporte){
+		res.status(400);
+	}else{
+		connection.query("INSERT INTO deportes (desc_deporte) VALUES ('"+req.body.desc_deporte+"')", function(error, result){
+			if (!!error){
+				console.log(error);
+			}else{
+				result.message = "Deporte agregado exitosamente";
+				res.json(result);
+			}
+		});		
+	}	
+});
+*/
+
+app.get('/deportes/:id', async (req, res) => {
+    connection.query("SELECT * FROM deportes where id_deporte="+req.params.id, function(error, rows, fields){
 		if (!!error){
 			console.log(error);
 		}else{
-			console.log(rows);
+			res.json(rows);
 		}
 	});
-    //res.json(clients);
 });
+
+/*
+app.get('/deportesbyid', async (req, res) => {
+    connection.query("SELECT * FROM deportes where id_deporte = "+req.body.id, function(error, rows, fields){
+		if (!!error){
+			console.log(error);
+		}else{
+			res.json(rows);
+		}
+	});
+});
+*/
 
 //CANALES
 app.get('/canales', async (req, res) => {
 
-    connection.query("SELECT * FROM tpup.canales ORDER BY nro_canal", function(error, rows, fields){
+    connection.query("SELECT * FROM canales ORDER BY nro_canal", function(error, rows, fields){
 		if (!!error){
 			console.log(error);
 		}else{
-			console.log(rows);
+			res.json(rows);
 		}
 	});
-    //res.json(clients);
 });
 
-//LUGARES//CANALES
+//LUGARES
 app.get('/lugares', async (req, res) => {
 
     connection.query("SELECT * FROM tpup.lugares ORDER BY id_lugar", function(error, rows, fields){
 		if (!!error){
 			console.log(error);
 		}else{
-			console.log(rows);
+			res.json(rows);
 		}
 	});
-    //res.json(clients);
 });
 
 
 // start server
-
 app.listen(process.env.PORT || 3000, function () {
-
     console.log('API andando con express...');
-
 });
